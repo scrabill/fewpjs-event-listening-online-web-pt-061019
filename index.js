64db3f106420c2ffb3a179dc9a6f68a8b17f5dde
scrabill <shannon.crabill@gmail.com>
2019-12-27 17:11:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+// Can this be refactored to reference another function?
+
 function addingEventListener() {
+  const input = document.getElementById('input');
+  input.addEventListener('click', function(event) {
+    alert('I was clicked!');
+  });
 }
